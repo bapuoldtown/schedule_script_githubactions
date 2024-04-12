c6f4af63275068a5f1450b9c37eb01f602c63eff
bapuoldtown <bapucodetest1987@gmail.com>
2024-04-12 23:25:28 +0530

    adding second commit

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-import requests
 
-url = "https://api.coinlore.net/api/global/"
 
-payload={}
-headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+print("hello world!!!!")
